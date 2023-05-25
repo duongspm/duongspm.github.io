5ae666b0e2e3bf490c9a8d3fc0923d42d202f8a7
duongspm <tranvanduong10a22016bb@gmail.com>
2023-05-25 16:10:14 +0700

    update profile

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -20,3 +20,11 @@ document.write('<span class="counter">0</span>')
 for (y=0;y<(jcount.toString().length);y++)
 document.write('<span class="counter">'+jcount.toString().charAt(y)+'</span>')
 }
+/*==================== SHOW SCROLL UP ====================*/ 
+
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-Up class
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
